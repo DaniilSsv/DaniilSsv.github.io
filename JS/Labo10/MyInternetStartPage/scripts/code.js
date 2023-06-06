f32b80af878e015e7cb32d895b68a59bdb04bef3
Daniil Samsonov <daniil.samsonov@student.vives.be>
2023-06-06 22:07:13 +0200

    opl labo10

--- JS/Labo10/MyInternetStartPage/scripts/code.js
+++ JS/Labo10/MyInternetStartPage/scripts/code.js
@@ -1,75 +1,118 @@
+let arrHistory = [];
+
 const setup = () => {
-    document.getElementById("btnGo").addEventListener("click", go);
-    restore();
-}
-window.addEventListener("load", setup);
+    loadHistory();
 
-const go = () => {
-    let input = document.getElementById("commandoInput").value;
+    let btnGo = document.getElementById("btnGo");
+    btnGo.addEventListener("click", clickBtnGo);
+};
 
-    let controle = input.match("\/[tygi]{1} [a-z]*");
-    if(controle != null){
-        let zoek = input.slice(3);
-        let type = input.slice(1,2);
-        let url = getLink(type, zoek);
-        window.open(url);
-        createCardAndAppend()
+const clickBtnGo = () => {
+    let input = document.getElementById("commandoInput");
+    let matches = input.value.match("^/[tygi]{1} .*");
+    if (matches != null) {
+        let type = matches[0].charAt(1);
+        let query = matches[0].substring(3);
+        let link = getLink(type, query);
+
+        let historyObject = {
+            type: type,
+            query: query,
+            url: link,
+        };
+
+        window.open(link, "_blank");
+        input.value = "";
+        createCard(type, query, link);
+        arrHistory.push(historyObject);
+        storeHistory();
     } else {
-        window.alert("invalid command");
+        alert("Query niet juist");
     }
-}
+};
 
-let getLink = (type, zoek) => {
-    let url;
-    let title;
-    switch (type) {
-        case "t":
-            url = "https://twitter.com/hashtag/" + zoek;
-            title = "twitter";
-            createCardAndAppend(title, zoek, url);
-            store(title, zoek, url);
-            return url;
-        case "y":
-            url = "https://youtube.com/results?search_query=" + zoek;
-            title = "youtube";
-            createCardAndAppend(title, zoek, url);
-            store(title, zoek, url);
-            return url;
-        case "g":
-            url = "https://www.google.com/search?q=" + zoek;
-            title = "google";
-            createCardAndAppend(title, zoek,url);
-            store(title, zoek, url);
-            return url;
-        case "i":
-            url = "https://www.instagram.com/explore/tags/" + zoek + "/";
-            title = "instagram";
-            createCardAndAppend(title, zoek, url);
-            store(title, zoek, url);
-            return url;
+const storeHistory = () => {
+    let jsonArr = JSON.stringify(arrHistory);
+    localStorage.setItem("vives.be.history", jsonArr);
+};
+
+const loadHistory = () => {
+    let historyJson = localStorage.getItem("vives.be.history");
+    if (historyJson !== undefined) {
+        arrHistory = JSON.parse(historyJson);
+        arrHistory.forEach((obj) => {
+            createCard(obj.type, obj.query, obj.url);
+        });
     }
-}
+};
 
-const createCardAndAppend = (title, commandoSuffix, url) => {
-    let col4 = createElementWithClassName("div", "col-4");
-    let card = createElementWithClassName("div", "card", title);
-    card.classList.add(title.toLowerCase()+"-card");
+const createCard = (type, query, link) => {
+    let row = document.getElementsByClassName("row")[0];
+
+    let col = createElementWithClassName("div", "col-4");
+    row.appendChild(col);
+
+    let card = createElementWithClassName("div", `card ${getSocialmediaNameLower(type)}-card`);
+    col.appendChild(card);
 
     let cardBody = createElementWithClassName("div", "card-body");
-    let cardTitle = createElementWithClassNameAndText("h5", "card-title", title);
-    let cardText = createElementWithClassNameAndText("p", "card-text", commandoSuffix);
-    let linkGo = createLinkButton(url, "card-button");
-
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    cardBody.appendChild(linkGo);
     card.appendChild(cardBody);
-    col4.appendChild(card);
 
-    let row = document.querySelector("#resultContainer > .row");
+    let title = createElementWithClassNameAndText("h5", "card-title", getSocialmediaName(type));
+    cardBody.appendChild(title);
 
-    row.appendChild(col4);
-}
+    let content = createElementWithClassNameAndText("p", "card-text", query);
+    cardBody.appendChild(content);
+
+    let btn = createButton(link, type);
+    cardBody.appendChild(btn);
+};
+
+const getLink = (type, query) => {
+    let link = "";
+    switch (type) {
+        case "y":
+            link = "https://youtube.com/results?search_query=" + query;
+            break;
+        case "t":
+            link = "https://twitter.com/hashtag/" + query;
+            break;
+        case "g":
+            link = "https://www.google.com/search?q=" + query;
+            break;
+        case "i":
+            link = "https://www.instagram.com/explore/tags/" + query;
+            break;
+        default:
+            break;
+    }
+    return link;
+};
+
+const getSocialmediaName = (type) => {
+    let name = "";
+    switch (type) {
+        case "y":
+            name = "YouTube";
+            break;
+        case "t":
+            name = "Twitter";
+            break;
+        case "g":
+            name = "Google";
+            break;
+        case "i":
+            name = "Instagram";
+            break;
+        default:
+            break;
+    }
+    return name;
+};
+
+const getSocialmediaNameLower = (type) => {
+    return getSocialmediaName(type).toLowerCase();
+};
 
 const createElementWithClassName = (element, className) => {
     let e = document.createElement(element);
@@ -77,47 +120,19 @@ const createElementWithClassName = (element, className) => {
     return e;
 };
 
-
-const createElementWithClassNameAndText = (element, className, text) => {
-    let e = createElementWithClassName(element, className);
-    e.appendChild(document.createTextNode(text))
+const createButton = (link, type) => {
+    let e = document.createElement("a");
+    e.setAttribute("class", `btn btn-primary ${getSocialmediaNameLower(type)}-button`);
+    e.setAttribute("href", link);
+    e.setAttribute("target", "_blank");
+    e.appendChild(document.createTextNode("Go!"));
     return e;
 };
 
-const createLinkButton = (url) => {
-    let linkGo = document.createElement("a");
-    linkGo.setAttribute("href", url);
-    linkGo.setAttribute("target", "_blank");
-    linkGo.setAttribute("class", "btn btn-primary");
-    linkGo.appendChild(document.createTextNode("Go!"));
-    return linkGo;
-}
+const createElementWithClassNameAndText = (element, className, text) => {
+    let e = createElementWithClassName(element, className);
+    e.appendChild(document.createTextNode(text));
+    return e;
+};
 
-const store = (title, text, url) => {
-    let hJSON = {
-        title: title,
-        text: text,
-        url:  url
-    };
-    let hString = JSON.stringify(hJSON);
-
-    let history;
-    let historyJSON = localStorage.getItem("labo-10.history");
-    if(historyJSON === null){
-        history = [];
-    } else {
-        history = JSON.parse(historyJSON);
-    }
-    history.push(hString);
-    localStorage.setItem("labo-10.history", JSON.stringify(history));
-}
-
-const restore = () => {
-    let history = localStorage.getItem("labo-10.history");
-    let historyJSON = JSON.parse(history);
-    //console.log("succes");
-    for(let i = 0; i < historyJSON.length; i++){
-        console.log(historyJSON[i].title);
-        createCardAndAppend(historyJSON[i].title, historyJSON[i].text, historyJSON[i].url);
-    }
-}
+window.addEventListener("load", setup);
